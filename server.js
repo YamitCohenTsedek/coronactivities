18e562fb8f6dd727589422f4a813c0a2302911bb
yamit.ct <yamit.ct@gmail.com>
2020-11-19 23:07:35 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const activityRouter = require('./routes/activities')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use('activities', activityRouter)
+app.use(express.static("./resources"));
+
+app.get('/', (req, res) => {
+    const activities = [{
+        title: 'Test title',
+        creationDate: new Date(),
+        category: 'test category',
+        description: 'test description'
+    }]
+    res.render('index', {activities: activities})
+})
+app.listen(5400)
