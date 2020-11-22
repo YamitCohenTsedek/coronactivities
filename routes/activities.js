875f78d87f81b8e47ca00be00bed407acdb08253
yamit.ct <yamit.ct@gmail.com>
2020-11-22 10:03:54 +0200

    Saving & canceling of New Activity.

--- routes/activities.js
+++ routes/activities.js
@@ -1,14 +1,43 @@
+// express is our web framework for Node.js.
 const express = require('express')
+// Import the activity model.
+const Activity = require('./../models/activity')
+// A router that can be used to create views.
 const router = express.Router()
 
-router.get('/new', (req, res) => {
-    const categories = ['Cooking', 'Sport', 'Crafts', 'Music', 'Reading', 'Home Organization', 'Games', 'Other']
-    categories.sort()
-    res.render('activities/new', { categories: categories })
+const categories = ['Cooking', 'Sport', 'Crafts', 'Music', 'Reading', 'Home Organization', 'Games', 'Other']
+categories.sort()
+
+router.get('/new', (req, res) => {    
+    res.render('activities/new', { activity: new Activity(), categories: categories })
 })
 
-router.post('/', (req, res) => {
+// Whenever we pass a route that has activity/{id}, the following code will be executed.
+router.get('/:id', async (req, res) => {
+    const activity = await Activity.findById(req.params.id)
+    // If the given id doesn't exist, go back to the home page. 
+    if (activity == null) {
+        res.redirect('/')
+    }
+})
 
+// Whenever we submit the New Activity form, the following code will be executed.
+router.post('/', async (req, res) => {
+    let activity = new Activity({
+        title: req.body.title,
+        category: req.body.category,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try {
+        // Save the new activity.
+        activity = await activity.Save()
+        // Redirect to the page of the saved activity.
+        res.redirect('/activities/${activity.id}')
+    // If an error oocurred, render out the page we were on. 
+    } catch (err) {
+        res.render('activities/new', { activity: activity, categories: categories })
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,19 +1,37 @@
+// express is our web framework for Node.js.
 const express = require('express')
+// A MongoDB object modeling tool designed to work in an asynchronous environment.
+const mongoose = require('mongoose')
+// Access to the router which was created in the specified file.
 const activityRouter = require('./routes/activities')
 const app = express()
 
+// Connect to the database.
+mongoose.connect('mongodb://localhost/coronactivities', { useUnifiedTopology:true, useNewUrlParser: true})
+
+// We will write our views using ejs and our view engine will convert that ejs code to HTML.
+// ejs lets you generate HTML markup with plain JavaScript.
 app.set('view engine', 'ejs')
-app.use('/activities', activityRouter)
+
+// Enable access to all the parameters of the activity form from the activity route by req.body.
+app.use(express.urlencoded({ extended: false}))
 app.use(express.static("./resources"));
 
+// The index/main route.
 app.get('/', (req, res) => {
     const activities = [{
         title: 'Storytelling I Spy',
         creationDate: new Date(),
         category: 'Games',
         description: 'If you enjoy a bit of honest misdirection and like to keep your friends guessing, this is for you. Perfect for people with a flair for drama, this game is another great way to learn a bit more about each player, whether you discover that they’re a master of deception or that they happen to own a secret snow globe collection.',
-        markdown: 'To play, each person takes a turn picking an object in their house and telling the story of how they got it, without actually saying what it is. The rest of the group then has to guess, as quickly as they can, what that object is. Depending on how challenging you want the game to be, you can keep it light by only recounting the origin stories of items most people probably have in their home, or you can lift the restriction and let players pick from any of their possessions.While you aren’t allowed to outright lie, the more dramatic, vague or misleading the storytelling, the better the game. If you’re stumped on what to say, you can also get creative: For a secondhand sweater, a friend once described what she imagined its former owner was like and the places they might have taken it.'
+        markdown: 'To play, each person takes a turn picking an object in their house and telling the story of how they got it, without actually saying what it is. The rest of the group then has to guess, as quickly as they can, what that object is. Depending on how challenging you want the game to be, you can keep it light by only recounting the origin stories of items most people probably have in their home, or you can lift the restriction and let players pick from any of their possessions.'
     }]
+    // Access to specified path from the views folder, with the given options.
     res.render('activities/index', {activities: activities})
 })
+
+// Use the activity router, and specify the path on which it is going to be based.
+app.use('/activities', activityRouter)
+
+// Listen for connections.
 app.listen(5400)
